45502bf2f9c36f770b4971c56d5acfcfbd55df9c
ptrcnull <git@ptcnull.me>
2020-04-04 23:45:59 +0200

    backend: Cleaned up get functions

--- backend/src/routes/merchants.ts
+++ backend/src/routes/merchants.ts
@@ -2,12 +2,5 @@ import { ExtendableContext } from 'koa'
 import { User } from '../entity/User'
 
 export default async function merchants(ctx: ExtendableContext) {
-  let m:any = []
-  const repo = ctx.getRepo(User)
-  let results = await repo.find({where: { isMerchant: true }})
-  results.forEach(function(merchant) {
-    m.push({id: merchant.id, email: merchant.email, name: merchant.name, location: merchant.location})
-  })
-  ctx.status = 200
-  ctx.message = JSON.stringify(m)
+  ctx.body = await ctx.getRepo(User).find({ isMerchant: true })
 }
--- backend/src/routes/models.ts
+++ backend/src/routes/models.ts
@@ -2,12 +2,5 @@ import { ExtendableContext } from 'koa'
 import { Model } from '../entity/Model'
 
 export default async function models(ctx: ExtendableContext) {
-  let m:any = []
-  const repo = ctx.getRepo(Model)
-  let results = await repo.find()
-  results.forEach(function(model) {
-    m.push({name: model.name, description: model.description, stlFile: model.stlFile, pictureURI: model.pictureURI})
-  })
-  ctx.status = 200
-  ctx.message = JSON.stringify(m)
+  ctx.body = await ctx.getRepo(Model).find()
 }
