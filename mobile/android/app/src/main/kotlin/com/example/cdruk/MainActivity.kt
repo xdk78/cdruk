e6b969b3edef9b2c4513edf9858fed11889ecc5a
unweek <unweek@protonmail.com>
2020-04-04 09:25:17 +0200

    mobile: initial

---
+++ mobile/android/app/src/main/kotlin/com/example/cdruk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cdruk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CDRUK',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CDRUK"),
+      ),
+      body: Center(
+        child: Text("Tu coś będzie")
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        tooltip: 'Add',
+        child: Icon(Icons.create),
+      ),
+    );
+  }
+}
