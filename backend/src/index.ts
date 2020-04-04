539cbc003896ecc8603424f5e58b6f3a14ef4234
Dominika <dominika@sakamoto.pl>
2020-04-04 11:45:38 +0200

    more **fun** dummy API routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
 
 const koa = new Koa();
 const router = new Router();
@@ -9,9 +10,31 @@ router
     ctx.body = {
       data: 'hello world!',
     };
-  });
+  })
+  .post('/register', (ctx) => {
+    ctx.body = ctx.request.body;
+  })
+  .post('/login', (ctx) => {
+    ctx.body = ctx.request.body;
+  })
+  .post('/getObjects', (ctx) => {
+    ctx.body = ctx.request.body;
+  })
+  .post('/getMerchants', (ctx) => {
+    ctx.body = ctx.request.body;
+  })
+  .post('/getMerchantQueue', (ctx) => {
+    ctx.body = ctx.request.body;
+  })
+  .post('/order', (ctx) => {
+    ctx.body = ctx.request.body;
+  })
+  .post('/getOrder', (ctx) => {
+    ctx.body = ctx.request.body;
+  })
 
 koa
   .use(router.routes())
   .use(router.allowedMethods())
+  .use(bodyParser())
   .listen(3000);
